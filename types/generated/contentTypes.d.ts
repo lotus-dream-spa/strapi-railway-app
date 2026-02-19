86fff1251a7edf8dbad6d332bfcae1cf914db9a4
lotusdreamspa <lotusdreamspa.sr@gmail.com>
2026-02-19 10:17:01 +0700

    added masseus img and description (also khdescription)

--- types/generated/contentTypes.d.ts
+++ types/generated/contentTypes.d.ts
@@ -551,6 +551,9 @@ export interface ApiMasseuseMasseuse extends Struct.CollectionTypeSchema {
     createdAt: Schema.Attribute.DateTime;
     createdBy: Schema.Attribute.Relation<'oneToOne', 'admin::user'> &
       Schema.Attribute.Private;
+    description: Schema.Attribute.Text;
+    img: Schema.Attribute.Media<'images' | 'files' | 'videos' | 'audios'>;
+    khDescription: Schema.Attribute.Text;
     locale: Schema.Attribute.String & Schema.Attribute.Private;
     localizations: Schema.Attribute.Relation<
       'oneToMany',

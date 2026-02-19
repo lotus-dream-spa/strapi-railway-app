099bf3a32fc4a064ecc7e3190405662747e851a8
lotusdreamspa <lotusdreamspa.sr@gmail.com>
2026-02-19 12:19:04 +0700

    change port

--- config/plugins.js
+++ config/plugins.js
@@ -10,25 +10,11 @@ module.exports = ({ env }) => ({
       providerOptions: {
         accessKeyId: env("CF_ACCESS_KEY_ID"),
         secretAccessKey: env("CF_ACCESS_SECRET"),
-        /**
-         * `https://<ACCOUNT_ID>.r2.cloudflarestorage.com`
-         */
         endpoint: env("CF_ENDPOINT"),
         params: {
           Bucket: env("CF_BUCKET"),
         },
-        /**
-         * Set this Option to store the CDN URL of your files and not the R2 endpoint URL in your DB.
-         * Can be used in Cloudflare R2 with Domain-Access or Public URL: https://pub-<YOUR_PULIC_BUCKET_ID>.r2.dev
-         * This option is required to upload files larger than 5MB, and is highly recommended to be set.
-         * Check the cloudflare docs for the setup: https://developers.cloudflare.com/r2/data-access/public-buckets/#enable-public-access-for-your-bucket
-         */
         cloudflarePublicAccessUrl: env("CF_PUBLIC_ACCESS_URL"),
-        /**
-         * Sets if all assets should be uploaded in the root dir regardless the strapi folder.
-         * It is useful because strapi sets folder names with numbers, not by user's input folder name
-         * By default it is false
-         */
         pool: false,
       },
       actionOptions: {
@@ -38,13 +24,13 @@ module.exports = ({ env }) => ({
       },
     },
   },
-   email: {
+  email: {
     config: {
-      provider: env('EMAIL_PROVIDER', 'nodemailer'), // Legge dalla variabile o usa nodemailer di default
+      provider: env('EMAIL_PROVIDER', 'nodemailer'),
       providerOptions: {
         host: env('EMAIL_SMTP_HOST', 'smtp.gmail.com'),
-        port: env.int('EMAIL_SMTP_PORT', 465),
-        secure: true,
+        port: env.int('EMAIL_SMTP_PORT', 587), // Usiamo 587 per evitare QDISC_DROP
+        secure: false, // TLS
         auth: {
           user: env('EMAIL_SMTP_USER'),
           pass: env('EMAIL_SMTP_PASS'),
